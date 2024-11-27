b6aec17e4902abcf0d53bd315450b3fd462bced0
Abhay <abhaysharma.developer@gmail.com>
2024-11-27 18:01:32 +0530

    Get video details

--- src/router/videoRouter.ts
+++ src/router/videoRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getVideoFeed, uploadVideo } from '../controllers/videoController';
+import { getVideoDetails, getVideoFeed, uploadVideo } from '../controllers/videoController';
 import { authenticationMiddleware } from '../middleware/authenticate';
 
 const videoRouter = Router()
 
 videoRouter.get('/feed', getVideoFeed);
 videoRouter.post('/upload', authenticationMiddleware, uploadVideo);
+videoRouter.get('/api/videos/:video_id', authenticationMiddleware, getVideoDetails)
 
 export default videoRouter
